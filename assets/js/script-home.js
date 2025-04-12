5f42e74cb015cb4a8443a3368f29774268bae942
S4mu3l-Silv4 <samuelssone02@gmail.com>
2025-04-11 21:37:41 -0300

    Subindo projeto pro GitHub

---
+++ assets/js/script-home.js
@@ -0,0 +1,13 @@
+document.getElementById('formulario').addEventListener('submit', function(e) {
+
+    const nome = document.getElementById('nome').value
+    const email = document.getElementById('email').value
+    const servico = document.getElementById('servico').value
+
+    const numero_celular = '5571997234881'
+    const mensagem = `Olá! Meu nome é ${nome}, meu e-mail é ${email} e eu gostaria de solicitar o seguinte serviço: ${servico}`
+    const numero_da_mensagem = encodeURIComponent(mensagem);
+    const url_whatsapp = `https://wa.me/${numero_celular}?text=${numero_da_mensagem}`
+
+    window.open(url_whatsapp, '_blank')
+});

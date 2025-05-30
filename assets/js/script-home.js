eb4d4e95058f4ecaecc30abd7b79f38f03dd016c
S4mu3l-Silv4 <samuelssone02@gmail.com>
2025-05-30 16:59:30 -0300

    Teste

--- assets/js/script-home.js
+++ assets/js/script-home.js
@@ -1,13 +1,15 @@
-document.getElementById('formulario').addEventListener('submit', function(e) {
+// Envio do formulário respondido ao WhatsApp:
+    
+    document.getElementById('formulario').addEventListener('submit', function(e) {
 
-    const nome = document.getElementById('nome').value
-    const email = document.getElementById('email').value
-    const servico = document.getElementById('servico').value
+        const nome = document.getElementById('nome').value
+        const email = document.getElementById('email').value
+        const servico = document.getElementById('servico').value
 
-    const numero_celular = '5571997234881'
-    const mensagem = `Olá, Samuel! Meu nome é ${nome}, meu e-mail é ${email} e eu gostaria de solicitar o seguinte serviço: ${servico}`
-    const numero_da_mensagem = encodeURIComponent(mensagem);
-    const url_whatsapp = `https://wa.me/${numero_celular}?text=${numero_da_mensagem}`
+        const numero_celular = '5571997234881'
+        const mensagem = `Olá, Samuel! Meu nome é ${nome}, meu e-mail é ${email} e eu gostaria de solicitar o seguinte serviço: ${servico}`
+        const numero_da_mensagem = encodeURIComponent(mensagem);
+        const url_whatsapp = `https://wa.me/${numero_celular}?text=${numero_da_mensagem}`
 
-    window.open(url_whatsapp, '_blank')
-});
+        window.open(url_whatsapp, '_blank')
+    })
